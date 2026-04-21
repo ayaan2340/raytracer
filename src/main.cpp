de1ade0bb11b182adb694df9d95b2aa3f29706dc
ayaan2340 <85899570+ayaan2340@users.noreply.github.com>
2026-04-20 23:42:22 -0500

    Adding rays and camera

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,53 @@
+#include "color.h"
+#include "vec3.h"
+#include "ray.h"
 #include <iostream>
+#include <algorithm>
+
+color ray_color(const ray& r) {
+    return color(0, 0.5 * (unit_vector(r.direction()).y() + 1.0), 0);
+}
+
 
 int main() {
 
-    constexpr int image_width { 256 };
-    constexpr int image_height { 256 };
+    // Image
+    constexpr auto aspect_ratio {16.0 / 9.0};
+    constexpr int image_width {400};
+    constexpr int image_height {std::max(1, static_cast<int>(image_width / aspect_ratio))};
 
+    // Camera
+    constexpr double viewport_height {2.0};
+    constexpr double viewport_width {viewport_height * (double(image_width) / image_height)};
+    constexpr double focal_length {1.0};
+    constexpr point3 camera_center {0, 0, 0};
+
+    // Horizontal (u) and vertical (v) viewport vectors
+    constexpr vec3 viewport_u {viewport_width, 0, 0};
+    constexpr vec3 viewport_v {0, -viewport_height, 0};
+
+    // Horizontal (u) and vertical (v) pixel delta vectors
+    constexpr vec3 pixel_delta_u {viewport_u / image_width};
+    constexpr vec3 pixel_delta_v {viewport_v / image_height};
+
+    // Location of upper left pixel
+    constexpr point3 viewport_upper_left {camera_center 
+                                         - vec3(0, 0, focal_length) 
+                                         - viewport_u / 2 - viewport_v / 2};
+    constexpr point3 pixel00_location {viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v)};
+
+    // Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width - 1);
-            auto g = 0.0;
-            auto b = double(j) / (image_height - 1);
+            auto pixel_center {pixel00_location + (i * pixel_delta_u) + (j * pixel_delta_v)};
+            auto ray_direction {pixel_center - camera_center};
+            ray r {camera_center, ray_direction};
 
-            int ir = int(255.99 * r);
-            int ig = g;
-            int ib = int(255.99 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            color pixel_color {ray_color(r)};
+            write_color(std::cout, pixel_color);
         }
     }
     std::clog << "\rDone.                 \n";
---
+++ src/vec3.h
@@ -0,0 +1,100 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <cmath>
+#include <iostream>
+#include <array>
+
+class vec3 {
+    public:
+        std::array<double, 3> e = {0, 0, 0};
+        
+        constexpr vec3() = default;
+        constexpr vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+
+        constexpr double x() const { return e[0]; }
+        constexpr double y() const { return e[1]; }
+        constexpr double z() const { return e[2]; }
+
+        constexpr vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+        constexpr double operator[](int i) const { return e[i]; }
+        constexpr double& operator[](int i) { return e[i]; }
+
+        constexpr vec3& operator+=(const vec3& v) {
+            e[0] += v.e[0];
+            e[1] += v.e[1];
+            e[2] += v.e[2];
+            return *this;
+        }
+
+        constexpr vec3& operator-=(const vec3& v) {
+            e[0] -= v.e[0];
+            e[1] -= v.e[1];
+            e[2] -= v.e[2];
+            return *this;
+        }
+
+        constexpr vec3& operator*=(double t) {
+            e[0] *= t;
+            e[1] *= t;
+            e[2] *= t;
+            return *this;
+        }
+
+        constexpr vec3& operator/=(double t) {
+            return *this *= 1/t;
+        }
+
+        constexpr double length() const {
+            return std::sqrt(length_squared());
+        }
+
+        constexpr double length_squared() const {
+            return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+        }
+
+};
+
+using point3 = vec3;
+
+inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+constexpr inline vec3 operator+(const vec3& v1, const vec3& v2) {
+    return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
+}
+
+constexpr inline vec3 operator-(const vec3& v1, const vec3& v2) {
+    return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);
+}
+
+constexpr inline vec3 operator*(const vec3& v, double t) {
+    return vec3(v.e[0] * t, v.e[1] * t, v.e[2] * t);
+}
+
+constexpr inline vec3 operator*(double t, const vec3& v) {
+    return v * t;
+}
+
+constexpr inline vec3 operator/(const vec3& v, double t) {
+    return (1/t) * v;
+}
+
+constexpr inline double dot(const vec3& v1, const vec3& v2) {
+    return v1.e[0] * v2.e[0]
+         + v1.e[1] * v2.e[1]
+         + v1.e[2] * v2.e[2];
+}
+
+constexpr inline vec3 cross(const vec3& v1, const vec3& v2) {
+    return vec3( v1.e[1] * v2.e[2] - v1.e[2] * v2.e[1], 
+                 v1.e[2] * v2.e[0] - v1.e[0] * v2.e[2],
+                 v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0]);
+}
+
+constexpr inline vec3 unit_vector(const vec3& v) {
+    return v / v.length();
+}
+
+#endif
